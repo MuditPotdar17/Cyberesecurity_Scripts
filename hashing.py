980fe7ba135f28667762a8b2a04ba57f9bfc43b5
MuditPotdar <122390204+MuditPotdar17@users.noreply.github.com>
2023-07-19 14:45:56 +0530

    Updated_hashing.py

--- hashing.py
+++ hashing.py
@@ -3,6 +3,8 @@ print("Convert text to md5:")
 print("Convert text to sha256:")
 print("Convert text to sha512")
 print("Convert text to blake2b ")
+print('Convert Text to sha3_256:')
+print('Convert text to sha3_512')
 n = int(input("Enter the choice of script to convert:"))
 if n==1:
     str = input("Enter your string to convert into hash")
@@ -12,13 +14,13 @@ if n==1:
 
 elif n==2:
     str = input("Enter your string to convert into hash")
-    result = hashlib.sha3_256(str.encode())
+    result = hashlib.sha256(str.encode())
     print("The hexadecimal hash is:",end="")
     print(result.hexdigest())
 
 elif n==3:
     str = input("Enter your string to convert into hash")
-    result = hashlib.sha3_512(str.encode())
+    result = hashlib.sha512(str.encode())
     print("The hexadecimal hash is :",end="")
     print(result.hexdigest())
 
@@ -28,4 +30,15 @@ elif n==4:
     print("The hexadecimal hash is :",end="")
     print(result.hexdigest())
 
+elif n==5:
+    str=input("Enter your string to convert into hash")
+    result = hashlib.sha3_256(str.encode())
+    print("The hexadecimal hash is:",end="")
+    print(result.hexdigest())
+
+elif n==6:
+    str=input("Enter your string to convert into hash")
+    result = hashlib.sha3_512(str.encode())
+    print("The hexadecimal hash is:",end="")
+    print(result.hexdigest())
 print("Program ended successfully")
